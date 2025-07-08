34d6eadb05a8201ddeaba4bbce324c32d3484ecd
Sunitha Obblareddy <sunitha.obblareddy@gmail.com>
2025-07-08 15:54:10 -0600

    June 24th homework

--- Script.js
+++ Script.js
@@ -1,7 +1,47 @@
-//import MyVar, {fibonacci as MyFunc, countDown} from "./recursion.js"
-//console.log(MyFunc(3)); // 55
-//console.log(MyVar); // ABC
-import * as file from "./recursion.js";
-console.log(file.fibonacci(3)); // 2    
-console.log(file.default); // ABC
-//DOM manipulation with plain javascript
+//multiplier factory code using closure
+function createMultiplier(factor) {
+
+function newfunction(number){
+    return number * factor;   
+}
+    return {
+        newfunction, createMultiplier,
+    };
+}
+const multiplier = createMultiplier(2);
+console.log(multiplier.newfunction(2)); // Output: 10
+
+//Factorial code using recursion
+function factorial(n) {
+  // base case
+  if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+console.log(factorial(6));
+//Guess List Application. Tried my best, but haven't completed fully 
+const guestInputName= document.getElementById("guestinput");
+    const addbtn = document.getElementById("addbtn");
+    const clearbtn = document.getElementById("clearbtn");
+    const list=document.getElementById("todoList")
+    const Removebtn=document.createElement("Remove");
+    Removebtn.addEventListener("click", ()=> {
+
+    });
+
+ addbtn.addEventListener("click", ()=>{
+    const guestname=document.getElementById("guestinput").value;
+    const guestlist=document.createElement("li");
+    guestlist.innerText=guestname;
+    list.append(guestlist, Removebtn );// add to the bottom of the list
+    //list. prepend(guestlist);//add to the top of the list
+
+  });
+
+
+
+
+
+
+
