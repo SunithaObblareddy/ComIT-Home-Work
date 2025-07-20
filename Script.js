bf460d4c490d2d7171161199f3f73ce0fb610a30
Sunitha Obblareddy <sunitha.obblareddy@gmail.com>
2025-07-20 03:17:52 -0600

    Todo app

--- Script.js
+++ Script.js
@@ -1,47 +1,69 @@
-//multiplier factory code using closure
-function createMultiplier(factor) {
+const dialog = document.getElementById("dialog");
+const showTodoFormBtn = document.getElementById("showTodoFormBtn");
+const todoList = document.getElementById("todoList");
+const todoForm = document.getElementById("todoForm");
+const closeDialogBtn = document.getElementById("closeDialogBtn");
+const textarea=document.getElementById("todo");
+const progressBar=document.getElementById("dialogclosingTimer");
+const closeDialogTime=5000;
+  let IntervalId;
+  let timeoutId;
+  const onDialogcloseHandler=()=>{
+    dialog.close();
+    clearInterval(IntervalId);  //stop a timer that was previously established using the setInterval() method
+    clearTimeout(timeoutId);
+    textarea.value="";
+  }
 
-function newfunction(number){
-    return number * factor;   
-}
-    return {
-        newfunction, createMultiplier,
-    };
-}
-const multiplier = createMultiplier(2);
-console.log(multiplier.newfunction(2)); // Output: 10
+showTodoFormBtn.addEventListener("click", ()=>{
+  dialog.showModal();
+    let timeLeft=closeDialogTime;
+  progressBar.max=closeDialogTime;
+  IntervalId=setInterval(() => {
+    console.log("executed");//Here fucntion is excuted n number of times so progress bar is updated everytime when we try to open form
+    timeLeft -= 100;
+    progressBar.value = timeLeft;
+  }, 100);
+    timeoutId=setTimeout(()=>{
+    textarea.value="";
+    onDialogcloseHandler();
+    timeLeft=closeDialogTime;
+  },closeDialogTime)
+  clear
+});
+closeDialogBtn.addEventListener("click", ()=>{
+  onDialogcloseHandler();
+    dialog.close();
 
-//Factorial code using recursion
-function factorial(n) {
-  // base case
-  if (n === 0 || n === 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
-}
-console.log(factorial(6));
-//Guess List Application. Tried my best, but haven't completed fully 
-const guestInputName= document.getElementById("guestinput");
-    const addbtn = document.getElementById("addbtn");
-    const clearbtn = document.getElementById("clearbtn");
-    const list=document.getElementById("todoList")
-    const Removebtn=document.createElement("Remove");
-    Removebtn.addEventListener("click", ()=> {
+});
 
-    });
+todoForm.addEventListener("submit", () => { 
+  const newTodoContainer = document.createElement("li");
+  const todoText = document.createElement("p");
+  const timestamp = document.createElement("p");
+  const deleteBtn = document.createElement("button");
 
- addbtn.addEventListener("click", ()=>{
-    const guestname=document.getElementById("guestinput").value;
-    const guestlist=document.createElement("li");
-    guestlist.innerText=guestname;
-    list.append(guestlist, Removebtn );// add to the bottom of the list
-    //list. prepend(guestlist);//add to the top of the list
+  deleteBtn.addEventListener("click", ()=> {
+    newTodoContainer.classList.add("fade-In")
+        newTodoContainer.classList.add("fade-Out")
+  setTimeout(()=>{
+    newTodoContainer.remove();
+  },300)
+});
 
-  });
-
-
-
+deleteBtn.innerHTML="delete";
+deleteBtn.classList.add("btn");//The classList property returns the CSS classnames of an element.
 
 
+  const currentDate = new Date();
 
+  todoText.innerHTML = textarea.value;
+  timestamp.innerHTML = currentDate.toLocaleDateString();
 
+  newTodoContainer.append(todoText, timestamp, deleteBtn);
+  todoList.prepend(newTodoContainer);
+
+onDialogcloseHandler();
+});
+
+//As a user I want a todo form dialog to be closed after a five seconds
