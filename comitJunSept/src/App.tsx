eb65f6253dc40da95b4598695716f2466dd32b9f
Sunitha Obblareddy <sunitha.obblareddy@gmail.com>
2025-07-31 15:24:39 -0600

    July 8th Home Work

--- comitJunSept/src/App.tsx
+++ comitJunSept/src/App.tsx
@@ -2,41 +2,45 @@ import './App.css';
 import { useState } from "react";
 
 // App Component
-function App() {
-  const [todoText, setTodoText] = useState("");//A variable to store the current text from the textarea, setTodoText ia a function to update the text
+function GuestList() {
+  const [GuestName, setGuestName] = useState("");//A variable to store the current text from the textarea, setTodoText ia a function to update the text
 
-  const [todos, setTodos] = useState<string[]>([]);//list array of all todo items, a function to addnew items to the list
+  const [guests, addGuests] = useState<string[]>([]);//list array of all todo items, a function to addnew items to the list
   //When user submits the form:
   const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();//stop the page from refreshing
-    setTodos((previousState) => [...previousState, todoText]);//adds new task to the previous state
-    setTodoText("");//clears textarea
+    addGuests((previousState) => [...previousState, GuestName]);//adds new task to the previous state
+    setGuestName("");//clears textarea
   };
 
-  const onTodoTextChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {//This function runs every time you type a letter.
-    setTodoText(event.target.value);//saves the typed text to todoText
+  const onGuestListChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {//This function runs every time you type a letter.
+    setGuestName(event.target.value);//saves the typed text to todoText
+  }
+  const onDeleteClickHandler = (index: number) => {
+  addGuests((previousState) => previousState.filter((_, i) => i !== index));
   };
 
-  //When user submits the form:
-
-  return (
+//JSX code//In react, components are executed only once not like();//to execute multiple times we use  useState("") hook
+return (
     <>
       <form onSubmit={onSubmitHandler}>
         <textarea
-          onChange={onTodoTextChangeHandler}
-          value={todoText}
+          onChange={onGuestListChangeHandler}
+          value={GuestName}
           placeholder="Enter todo text"
         ></textarea>
         <input type="submit" value="Add" />
       </form>
 
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+        {guests.map((guest, index) => (
+          <li key={index}>{guest}
+          <button onClick={()=>onDeleteClickHandler(index)}>Delete</button>
+          </li>
         ))}
       </ul>
     </>
   );
 }
 
-export default App;
+export default GuestList;
--- comitJunSept/src/main.tsx
+++ comitJunSept/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import GuestList from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(//Tells React to start the app in the HTML element with ID "root"
-  <StrictMode> //Helps catch errors and give warnings in development
-    <App />//main logic
+  <StrictMode> 
+    <GuestList/>
   </StrictMode>
 );
